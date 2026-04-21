52ffa4074c26e9c370a11e1a5263961efba1025a
Isidoros <duckisidoros@gmail.com>
2026-04-21 19:27:40 +0700

    Submission of Week 3 Adding

--- Assignments for Week 3/Assignment of 3-transport/3-visualize-location-data/api/maps-key/index.js
+++ Assignments for Week 3/Assignment of 3-transport/3-visualize-location-data/api/maps-key/index.js
@@ -1,7 +1,12 @@
 module.exports = async function (context, req) {
     context.res = {
         status: 200,
-        body: { key: process.env.AZURE_MAPS_KEY },
-        headers: { 'Content-Type': 'application/json' }
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: { 
+            key: process.env.AZURE_MAPS_KEY 
+        }
     };
 };
